92183027d77fe1e79bd30057896a03da7a6e46ec
tatsuya <yama.0623motsu@gmail.com>
2024-02-13 15:51:37 +0900

    update *_latent-space.py

--- wgan/wgan-gp-sc_latent-space.py
+++ wgan/wgan-gp-sc_latent-space.py
@@ -5,7 +5,6 @@ import seaborn as sns
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from sklearn.decomposition import PCA
-# from sklearn.manifold import TSNE
 from scipy.stats import pearsonr
 
 sys.path.append('.')
@@ -74,10 +73,6 @@ for dr in dropout:
     z_new = pca.fit_transform(X=z)
     evr = pca.explained_variance_ratio_ * 100
 
-    # # t-SNE
-    # tSNE = TSNE(n_components=2, perplexity=5, random_state=0)
-    # z_tsne = tSNE.fit_transform(X=z)
-
     # Plotting [Latent space: PCA]
     fig = plt.figure(figsize=(10, 7))
     ax = fig.add_subplot()
@@ -125,125 +120,5 @@ for dr in dropout:
 
     fig.savefig(f'./fig/wgan-gp-sc/training_latent_score_corr_pca_dr_{dr}.pdf', transparent=True)
     fig.savefig(f'./fig/wgan-gp-sc/training_latent_score_corr_pca_dr_{dr}.png', dpi=700)
-
-    # # Plotting [Latent space: t-SNE]
-    # fig = plt.figure(figsize=(10, 7))
-    # ax = fig.add_subplot()
-    # mappable = ax.scatter(z_tsne[:, 0], z_tsne[:, 1], c=target, edgecolors='#000000', cmap='OrRd', linewidth=1.0, s=50)
-    # ax.set_xlabel('t-SNE 1', fontsize=18)
-    # ax.set_ylabel('t-SNE 2', fontsize=18)
-    # # ax.set_title(f'[Training] Dropout: {dr}', fontsize=20)
-    # ax.grid(linestyle='dotted', linewidth=1.5)
-    #
-    # ax.spines["top"].set_linewidth(2.0)
-    # ax.spines["bottom"].set_linewidth(2.0)
-    # ax.spines["right"].set_linewidth(2.0)
-    # ax.spines["left"].set_linewidth(2.0)
-    #
-    # plt.xticks(fontsize=15)
-    # plt.yticks(fontsize=15)
-    #
-    # cbar = fig.colorbar(mappable, ax=ax)
-    # cbar.ax.tick_params(labelsize=15)
-    #
-    # fig.savefig(f'./fig/wgan-gp-sc/training_latent_space_tSNE_dr_{dr}.pdf', transparent=True)
-
-    # # ------ Synthesize connectivity matrix [Validation] ------ #
-    # for i, data in enumerate(valid_loader):
-    #     # Real matrix and target
-    #     matrix, target = data
-    #     real = matrix.to(dtype=torch.float32)
-    #     target = target.to(dtype=torch.float32)
-    #
-    #     # Synthesize connectivity matrix
-    #     z = encoder(real)
-    #     fake = decoder(z)
-    #
-    # z = z.detach().numpy()
-    # real = real.detach().numpy()
-    # real = real.reshape(real.shape[0], real.shape[-1], real.shape[-1])
-    # fake = fake.detach().numpy()
-    # fake = fake.reshape(fake.shape[0], fake.shape[-1], fake.shape[-1])
-    # target = target.detach().numpy()
-    # print('real: ', real.shape)
-    # print('fake: ', fake.shape)
-    #
-    # # Principal Component Analysis
-    # pca = PCA()
-    # z_new = pca.fit_transform(X=z)
-    # evr = pca.explained_variance_ratio_ * 100
-
-    # # t-SNE
-    # tSNE = TSNE(n_components=2, perplexity=5, random_state=0)
-    # z_tsne = tSNE.fit_transform(X=z)
-    #
-    # # Plotting
-    # fig = plt.figure(figsize=(10, 7))
-    # ax = fig.add_subplot()
-    # mappable = ax.scatter(z_new[:, 0], z_new[:, 1], c=target, edgecolors='#000000', cmap='OrRd', linewidth=1.0, s=50)
-    # ax.set_xlabel(f'PC 1 [{evr[0]:.2f}%]', fontsize=18)
-    # ax.set_ylabel(f'PC 2 [{evr[1]:.2f}%]', fontsize=18)
-    # # ax.set_title(f'[Validation] Dropout: {dr}', fontsize=20)
-    # ax.grid(linestyle='dotted', linewidth=1.5)
-    #
-    # ax.spines["top"].set_linewidth(2.0)
-    # ax.spines["bottom"].set_linewidth(2.0)
-    # ax.spines["right"].set_linewidth(2.0)
-    # ax.spines["left"].set_linewidth(2.0)
-    #
-    # plt.xticks(fontsize=15)
-    # plt.yticks(fontsize=15)
-    #
-    # cbar = fig.colorbar(mappable, ax=ax)
-    # cbar.ax.tick_params(labelsize=15)
-    #
-    # fig.savefig(f'./fig/wgan-gp-sc/validation_latent_space_pca_dr_{dr}.pdf', transparent=True)
-    # fig.savefig(f'./fig/wgan-gp-sc/validation_latent_space_pca_dr_{dr}.png', dpi=700)
-
-    # # Plotting [Latent space and Objective function: PCA]
-    # rval, pval = pearsonr(z_new[:, 0], target)
-    # print(f'[Validation] r: {rval:.2f}, p: {pval:.2f}')
-    #
-    # fig = plt.figure(figsize=(10, 7))
-    # ax = fig.add_subplot()
-    # mappable = ax.scatter(z_new[:, 0], target, c='#DC3220', edgecolors='#000000', linewidth=1.0, s=50)
-    # sns.regplot(x=z_new[:, 0], y=target, scatter=False, fit_reg=True, ci=0, ax=ax,line_kws={'color': '#DC3220', 'linewidth': 3.0})
-    # sns.regplot(x=z_new[:, 0], y=target, scatter=False, ax=ax, line_kws={'color': '#808080', 'linewidth': 0.0})
-    # ax.set_xlabel(f'PC 1 [{evr[0]:.2f}%]', fontsize=18)
-    # ax.set_ylabel(f'Fluid intelligence', fontsize=18)
-    # ax.grid(linestyle='dotted', linewidth=1.5)
-    #
-    # ax.spines["top"].set_linewidth(2.0)
-    # ax.spines["bottom"].set_linewidth(2.0)
-    # ax.spines["right"].set_linewidth(2.0)
-    # ax.spines["left"].set_linewidth(2.0)
-    #
-    # plt.xticks(fontsize=15)
-    # plt.yticks(fontsize=15)
-    #
-    # fig.savefig(f'./fig/wgan-gp-sc/validation_latent_score_corr_pca_dr_{dr}.pdf', transparent=True)
-    # fig.savefig(f'./fig/wgan-gp-sc/validation_latent_score_corr_pca_dr_{dr}.png', dpi=700)
-
-    # # Plotting [Latent space: t-SNE]
-    # fig = plt.figure(figsize=(10, 7))
-    # ax = fig.add_subplot()
-    # mappable = ax.scatter(z_tsne[:, 0], z_tsne[:, 1], c=target, edgecolors='#000000', cmap='OrRd', linewidth=1.0, s=50)
-    # ax.set_xlabel('t-SNE 1', fontsize=18)
-    # ax.set_ylabel('t-SNE 2', fontsize=18)
-    # # ax.set_title(f'[Validation] Dropout: {dr}', fontsize=20)
-    # ax.grid(linestyle='dotted', linewidth=1.5)
-    #
-    # ax.spines["top"].set_linewidth(2.0)
-    # ax.spines["bottom"].set_linewidth(2.0)
-    # ax.spines["right"].set_linewidth(2.0)
-    # ax.spines["left"].set_linewidth(2.0)
-    #
-    # plt.xticks(fontsize=15)
-    # plt.yticks(fontsize=15)
-    #
-    # cbar = fig.colorbar(mappable, ax=ax)
-    # cbar.ax.tick_params(labelsize=15)
-    #
-    # fig.savefig(f'./fig/wgan-gp-sc/validation_latent_space_tSNE_dr_{dr}.pdf', transparent=True)
 
 plt.show()
